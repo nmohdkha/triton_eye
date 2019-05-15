50b40a48ba596e5ac93939ef5af981f9764d79d6
ChrisBDavis <50641044+ChrisBDavis@users.noreply.github.com>
2019-05-15 14:46:29 -0700

    Update CLaWing_comp.py

--- CLaWing_comp.py
+++ CLaWing_comp.py
@@ -13,9 +13,9 @@ class CLaWingComp(ExplicitComponent):
         #self.add_input('Sexposed') function of S
         self.add_input('S')
         self.add_input('d_fuse')
-        self.add_input('b_wing')
+        self.add_input('b')
         self.add_output('CLa')
-        self.declare_partials('AR','M','Cla','S','d_fuse','b_wing','CLa',method='cs')
+        #self.declare_partials('AR','M','Cla','S','d_fuse','b_wing','CLa',method='cs')
         # do I need to declare partials if the values are constant? e.g. Cla
         # b_wing, AR, and S are not independent of each other...better to calc one below and
         # delete an input above?
@@ -29,9 +29,10 @@ class CLaWingComp(ExplicitComponent):
         #Sweepmaxt = inputs['Sweepmaxt']
         #Sexposed = inputs['Sexposed']
         d_fuse = inputs['d_fuse']
-        b_wing = inputs['b_wing']
-        F = 1.07*(1+d_fuse/b_wing)**2
+        b = inputs['b']
+        F = 1.07*(1+d_fuse/b)**2
 
         outputs['CLa'] = 2*np.pi*AR*((S*.9)/S)*F/(2 + np.sqrt(4 + AR**2*Beta**2/(neta**2)))
+
 
 
